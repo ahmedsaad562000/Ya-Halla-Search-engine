5bbdc755ac54433806ce75c3f1989f3d34a42c03
Mahmoud Khaled <104264297+mahmoudk07@users.noreply.github.com>
2023-05-02 22:54:34 +0300

    Class word document structure

---
+++ src/main/java/WordDocuement.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class WordDocuement
+{
+    String Word;
+    int DocumentFrequency;
+    double IDF;
+    List<URLDocument> URLS = new ArrayList<>();
+}
