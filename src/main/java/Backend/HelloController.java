bdd36a88e5f56c52271a11db53b199fb346eb357
ahmed <76961547+ahmedsaad562000@users.noreply.github.com>
2023-05-17 04:16:19 +0300

    search api added but needs modification

--- src/main/java/Backend/HelloController.java
+++ src/main/java/Backend/HelloController.java
@@ -1,9 +1,16 @@
 package Backend;
 
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.concurrent.atomic.AtomicLong;
+
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class HelloController {
@@ -16,6 +23,26 @@ public class HelloController {
     @ResponseBody
     public String getEmployeesById(@PathVariable String id) {
         return "ID: " + id;
+    }
+
+
+    // make a post request to /api/employees/{id}
+
+
+    @RestController
+    public static class MyController {
+
+        @PostMapping("/upload")
+        public HashMap<String, Double> upload(
+                             @RequestParam("search") String username , @RequestParam("phrase") boolean phrase) {
+            HashMap<String, Double> map = new HashMap<String, Double>();
+            map.put("link1", 1.0);
+            map.put("link2", 5.5);
+            map.put("link3", 9.9);
+            map.put("time", 0.0005);
+            return map;
+        }
+
     }
 
 }
