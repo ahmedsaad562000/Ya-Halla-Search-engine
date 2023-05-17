e9238215f209488694306f433bb272e451c9836b
ahmed <76961547+ahmedsaad562000@users.noreply.github.com>
2023-05-18 02:39:21 +0300

    Revert "indexer removed for merge"
    This reverts commit f699681f34ed44b393bab0435f16bb3fd58ea5bf.

---
+++ src/main/java/URLDocument.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class URLDocument
+{
+    String URL_name;
+    int TermFrequency;;
+    Integer Priority;
+    List<Integer> WordPosition = new ArrayList<>();
+    String firstParagraph;
+}
